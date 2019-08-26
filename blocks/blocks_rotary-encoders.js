4aeaa42c4a9485521f9beeea8a4c6d3702fce829
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 12:21:11 +0700

    add generator

--- blocks/blocks_rotary-encoders.js
+++ blocks/blocks_rotary-encoders.js
@@ -1,21 +1,30 @@
-Blockly.Blocks['rotary_encoder_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("ROTARY ENCODER");
-    this.appendValueInput("ROTARY_ENCODER_CLK_PIN")
-        .setCheck("Number")
-        .appendField("CLK PIN");
-    this.appendValueInput("ROTARY_ENCODER_DT_PIN")
-        .setCheck("Number")
-        .appendField("DT PIN");
-    this.appendValueInput("ROTARY_ENCODER_SW_PIN")
-        .setCheck("Number")
-        .appendField("SW PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["rotary_encoder_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("ROTARY ENCODER");
+      this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("CLK PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "ROTARY_ENCODER_CLK_PIN");
+      this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("DT PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "ROTARY_ENCODER_DT_PIN");
+      this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("SW PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "ROTARY_ENCODER_SW_PIN");
+      this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("ROTARY_ENCODER_VALUE"),
+          "ROTARY_ENCODER_VALUE"
+        );
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_rotary-encoders.js
+++ blocks/generators_rotary-encoders.js
@@ -1,8 +1,56 @@
-Blockly.JavaScript['rotary_encoder_block'] = function(block) {
-  var value_rotary_encoder_clk_pin = Blockly.JavaScript.valueToCode(block, 'ROTARY_ENCODER_CLK_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_rotary_encoder_dt_pin = Blockly.JavaScript.valueToCode(block, 'ROTARY_ENCODER_DT_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_rotary_encoder_sw_pin = Blockly.JavaScript.valueToCode(block, 'ROTARY_ENCODER_SW_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["rotary_encoder_block"] = function(block) {
+    var text_rotary_encoder_clk_pin = block.getFieldValue(
+      "ROTARY_ENCODER_CLK_PIN"
+    );
+    var text_rotary_encoder_dt_pin = block.getFieldValue(
+      "ROTARY_ENCODER_DT_PIN"
+    );
+    var text_rotary_encoder_sw_pin = block.getFieldValue(
+      "ROTARY_ENCODER_SW_PIN"
+    );
+    var variable_rotary_encoder_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("ROTARY_ENCODER_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+
+	#SETUP
+	//set clkPin,dePin,swPin as INPUT
+	pinMode(ROTARY_ENCODER_CLK_PIN, INPUT);
+	pinMode(ROTARY_ENCODER_DT_PIN, INPUT);
+	pinMode(ROTARY_ENCODER_SW_PIN, INPUT);
+	digitalWrite(ROTARY_ENCODER_SW_PIN, HIGH);
+	#END
+
+	#EXTINC
+	const int ROTARY_ENCODER_CLK_PIN = ${text_rotary_encoder_clk_pin}; //the clk attach to pin 2
+	const int ROTARY_ENCODER_DT_PIN = ${text_rotary_encoder_dt_pin}; //the dt pin attach to pin 3
+	const int ROTARY_ENCODER_SW_PIN = ${text_rotary_encoder_sw_pin} ;//the sw pin attach to pin 4
+
+	int getEncoderTurn(void)
+	{
+		static int oldA = HIGH; //set the oldA as HIGH
+		static int oldB = HIGH; //set the oldB as HIGH
+		int result = 0;
+		int newA = digitalRead(ROTARY_ENCODER_CLK_PIN);//read the value of clkPin to newA
+		int newB = digitalRead(ROTARY_ENCODER_DT_PIN);//read the value of dtPin to newB
+		if (newA != oldA || newB != oldB) //if the value of clkPin or the dtPin has changed
+		{
+		// something has changed
+			if (oldA == HIGH && newA == LOW)
+			{
+				result = (oldB * 2 - 1);
+			}
+		}
+		oldA = newA;
+		oldB = newB;
+		return result;
+	}
+	#END
+
+	int ROTARY_ENCODER_CHANGE = getEncoderTurn();//
+	${variable_rotary_encoder_value} = ${variable_rotary_encoder_value} + ROTARY_ENCODER_CHANGE;
+    `;
+    return code;
+  };
